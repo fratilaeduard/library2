a63e4a1f603fdff0b5b9c9c2d8b525cf3beb848b
fratilaeduard <155654647+fratilaeduard@users.noreply.github.com>
2024-04-10 19:30:11 +0300

    Add files via upload

---
+++ Library2Application.java
@@ -0,0 +1,14 @@
+package com.example.library2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Library2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Library2Application.class, args);
+	}
+
+}
